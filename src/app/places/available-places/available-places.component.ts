220ad7054ebb90c162f5aefaf77eda093d7fcda9
Shankar J <shankar.j@bthinq.com>
2024-10-18 11:47:20 +0530

    Learning api calls

---
+++ src/app/places/available-places/available-places.component.ts
@@ -0,0 +1,54 @@
+import { Component, signal, OnInit, DestroyRef, inject } from '@angular/core';
+
+import { Place } from '../place.model';
+import { PlacesComponent } from '../places.component';
+import { PlacesContainerComponent } from '../places-container/places-container.component';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs';
+import { PlacesService } from '../places.service';
+
+@Component({
+  selector: 'app-available-places',
+  standalone: true,
+  templateUrl: './available-places.component.html',
+  styleUrl: './available-places.component.css',
+  imports: [PlacesComponent, PlacesContainerComponent],
+})
+export class AvailablePlacesComponent implements OnInit {
+  places = signal<Place[] | undefined>(undefined);
+  isFetching = signal(false);
+  private placesService = inject(PlacesService);
+  private destroyRef = inject(DestroyRef);
+
+  ngOnInit() {
+    this.isFetching.set(true);
+    const subscription = this.placesService.loadAvailablePlaces().subscribe({
+      next: (places) => {
+        this.places.set(places);
+      },
+      complete: () => {
+        this.isFetching.set(false);
+      },
+    });
+
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    });
+  }
+
+  onSelectPlace(selectedPlace: Place) {
+   const subscription = this.placesService.addPlaceToUserPlaces(selectedPlace)
+      .subscribe({
+        next: (resData) => {
+          console.log("Place selected and added to user's favorites", resData);
+        },
+        // error: (error) => {
+        //   console.error('Error selecting place:', error);
+        // },
+      });
+
+      this.destroyRef.onDestroy(() => {
+        subscription.unsubscribe();
+      });
+  }
+}
